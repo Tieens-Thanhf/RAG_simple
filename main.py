9f1c435777746106ef333e836731f6447500d032
Tieens-Thanhf <184966458+Tieens-Thanhf@users.noreply.github.com>
2026-05-14 21:43:44 +0700

    Add files via upload

---
+++ main.py
@@ -0,0 +1,26 @@
+from langchain_ollama.llms import OllamaLLM
+from langchain_core.prompts import ChatPromptTemplate
+from vector import retriever
+
+model = OllamaLLM(model="llama3.2")
+
+template = """
+You are a helpful assistant that answers questions about the world.
+
+reviews: {reviews}
+
+question: {question}
+"""
+prompt = ChatPromptTemplate.from_template(template)
+chain = prompt | model
+
+while True:
+    print("\n\n------------------------------")
+    question = input("Ask your question (q to quit): ")
+    print("\n\n")
+    if question.lower() == "q":
+        break
+    
+    reviews = retriever.invoke(question)
+    result = chain.invoke({"reviews": reviews, "question": question})
+    print(result)
